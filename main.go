f2b13355ae5c5cd066b22306cea45c54bf6763f6
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-17 02:24:37 +0100

    test concurency

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -116,18 +117,33 @@ func removeClient(clientID string) {
 }
 
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	var wg sync.WaitGroup
 
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
+	wg.Add(2)
+
+	go func() {
+		test1()
+		wg.Done()
+	}()
+	go func() {
+		test2()
+		wg.Done() // Indique que test2 est terminé
+	}()
+
+	fmt.Println("zzsssqsqsqs")
+
+	// Attendre que toutes les goroutines aient terminé
+	wg.Wait()
+}
+
+func test1() {
+	for i := 0; i < 1111; i++ {
+		fmt.Println("test1 :", i)
 	}
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func test2() {
+	for i := 0; i < 222; i++ {
+		fmt.Println("test2 :", i)
+	}
+}
