613759015eb489225bd79140d24af8e86315e2c0
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-17 13:26:04 +0100

    add persist workers

---
+++ workers/persistence/persistence_worker.go
@@ -0,0 +1,13 @@
+package persistence
+
+import "chatApp/shared"
+
+type MessageRPCServer string
+
+func (t *MessageRPCServer) PersistMessage(msg shared.Message, reply *string) error {
+
+}
+
+func main() {
+
+}
